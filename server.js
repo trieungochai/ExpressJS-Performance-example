43caeb37ae71df35bb0f873552e210536a9a2152
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-06-20 15:46:39 +0700

    Using PM2 to create Clusters

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const cluster = require("cluster");
-const os = require("os");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,9 +12,6 @@ function delay(duration) {
 }
 
 app.get("/", (req, res) => {
-  // JSON.stringify({}) => "{}"
-  // JSON.parse("{}") => {}
-  // [5, 1, 2, 3, 4].sort()
   res.send(`Performance example! ${process.pid}`);
 });
 
@@ -27,13 +22,5 @@ app.get("/timer", (req, res) => {
 });
 
 console.log("Running server.js...");
-if (cluster.isMaster) {
-  console.log("Master has been started");
-  const NUM_WORKERS = os.cpus().length;
-  for (let i = 0; i < NUM_WORKERS; i++) {
-    cluster.fork();
-  }
-} else {
-  console.log("Worker process started");
-  app.listen(PORT);
-}
+console.log("Worker process started");
+app.listen(PORT);
