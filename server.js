43875511e437cddd7163d7ba423b77cf45c24af1
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-06-20 11:54:50 +0700

    Maximizing Cluster Performance

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cluster = require("cluster");
+const os = require("os");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -28,11 +29,11 @@ app.get("/timer", (req, res) => {
 console.log("Running server.js...");
 if (cluster.isMaster) {
   console.log("Master has been started");
-  cluster.fork();
-  cluster.fork();
+  const NUM_WORKERS = os.cpus().length;
+  for (let i = 0; i < NUM_WORKERS; i++) {
+    cluster.fork();
+  }
 } else {
   console.log("Worker process started");
   app.listen(PORT);
 }
-
-// app.listen(PORT);
